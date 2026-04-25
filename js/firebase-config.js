e12b7a68199bf9a1acfb26cd9a8de494d3368332
Yhas1417 <yusufkeskin1417@gmail.com>
2026-04-25 18:16:12 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
- 
+
 const firebaseConfig = {
   apiKey: "AIzaSyArIYOp-msUGe_T4bLiVkoDhvXYhLuWVeo",
   authDomain: "test-sitem.firebaseapp.com",
@@ -11,9 +11,9 @@ const firebaseConfig = {
   messagingSenderId: "97147105273",
   appId: "1:97147105273:web:6e70cf559735f68fb8d728"
 };
- 
-const app  = initializeApp(firebaseConfig);
+
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
-const db   = getFirestore(app);
- 
+const db = getFirestore(app);
+
 export { auth, db };
