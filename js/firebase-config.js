28e4403ab08f6edbbd90e8412ef06cf137a59596
Yhas1417 <yusufkeskin1417@gmail.com>
2026-04-25 16:35:03 +0300

    Create firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,20 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+ 
+const firebaseConfig = {
+  apiKey: "AIzaSyArIYOp-msUGe_T4bLiVkoDhvXYhLuWVeo",
+  authDomain: "test-sitem.firebaseapp.com",
+  databaseURL: "https://test-sitem-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "test-sitem",
+  storageBucket: "test-sitem.firebasestorage.app",
+  messagingSenderId: "97147105273",
+  appId: "1:97147105273:web:6e70cf559735f68fb8d728",
+  measurementId: "G-ETZLC279HD""
+};
+ 
+const app  = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db   = getFirestore(app);
+ 
+export { auth, db };
